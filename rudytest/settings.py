b598ca69d40dbe806da4257030770df31fba6c81
Rudy <drawncloser@gmail.com>
2015-02-02 12:04:47 -0600

    Django Rest Framework

--- rudytest/settings.py
+++ rudytest/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pfb9x=1r!vi*&$jmo!_6d!2vl88v74nwa=cytk4&x3+6ba#5tt'
+SECRET_KEY = os.environ.get('SECRET_KEY', "===SECRET KEY GOES HERE===").strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
