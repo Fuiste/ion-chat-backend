83bfa9cb9b0b7248f191dc63e020af8d9de8e38d
Rudy <drawncloser@gmail.com>
2015-02-03 10:31:36 -0600

    SUPER basic API

--- app/serializers.py
+++ app/serializers.py
@@ -51,3 +51,45 @@ class ChatterList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MessageDetail(APIView):
+    model = Message
+    serializer_class = MessageSerializer
+
+    def get_object(self, pk):
+        try:
+            return Message.objects.get(pk=pk)
+        except Message.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        message = self.get_object(pk)
+        serializer = MessageSerializer(message)
+        return Response(serializer.data)
+
+
+class ChatterDetail(APIView):
+    model = Chatter
+    serializer_class = ChatterSerializer
+
+    def get_object(self, pk):
+        try:
+            return Chatter.objects.get(pk=pk)
+        except Chatter.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        chatter = self.get_object(pk)
+        serializer = ChatterSerializer(chatter)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        if "id" not in request.DATA:
+             request.DATA["id"] = pk
+        chatter = self.get_object(pk)
+        serializer = ChatterSerializer(chatter, data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- rudytest/urls.py
+++ rudytest/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from app.serializers import *
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'rudytest.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/messages/$', MessageList.as_view(), name='messages-list'),
+    url(r'^api/messages/(?P<pk>[0-9]+)/$', MessageDetail.as_view(), name='messages-detail'),
+    url(r'^api/chatters/$', MessageList.as_view(), name='chatters-list'),
+    url(r'^api/chatters/(?P<pk>[0-9]+)/$', MessageDetail.as_view(), name='chatters-detail'),
 )
