0585d6c4e9f4a579e0c12c7f77e0cf0a5ef7229e
Rudy <drawncloser@gmail.com>
2015-02-03 15:13:08 -0600

    This might actually work

--- app/serializers.py
+++ app/serializers.py
@@ -47,8 +47,7 @@ class EmailUserObtainAuthToken(APIView):
     def post(self, request):
         serializer = self.serializer_class(data=request.DATA)
         if serializer.is_valid():
-            token, created = Token.objects.get_or_create(user=serializer.object['user'])
-            return Response({'token': token.key})
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
